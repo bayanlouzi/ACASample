817401be7e1ac900b730fbcce58615f08fd24e84
bayanlouzi <93653056+bayanlouzi@users.noreply.github.com>
2023-03-22 16:02:06 +0300

    Create deploy-arm.ps1

---
+++ container-apps-revisions/deploy-arm.ps1
@@ -0,0 +1,8 @@
+$grp = "ca-session-rg"
+$loc = "eastus"
+
+
+az group create --name $grp --location $loc
+                
+
+az deployment group create --resource-group $grp --template-file 'template.json'
