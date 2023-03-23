0b5497f6e319d8e224a16826993128d8b8c35ae0
bayanlouzi <93653056+bayanlouzi@users.noreply.github.com>
2023-03-23 11:05:16 +0300

    Delete container-apps-revisions directory

--- container-apps-revisions/deploy-arm.ps1
+++
@@ -1,8 +0,0 @@
-$grp = "ca-session-rg"
-$loc = "eastus"
-
-
-az group create --name $grp --location $loc
-                
-
-az deployment group create --resource-group $grp --template-file 'template.json'
